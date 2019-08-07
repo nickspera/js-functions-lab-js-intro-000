2802d22de705d0a1eb7c6e7d25eee81cc246c278
Nick Spera <nickaspera@gmail.com>
2019-08-07 16:14:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ function happyHolidaysTo(name) {
 
 function happyHolidayTo(holiday, name) {
   return ("Happy ${holiday}, ${name}!")
-  happyHolidayTo('Independence Day', 'you')
 }
-
+  happyHolidayTo('Independence Day', 'you')
 
 function holidayCountDown(holiday, days) {
   return ("It\'s ${days} until ${holiday}!")
-  holidayCountDown("Mother's Day", 20)
 }
+  holidayCountDown("Mother's Day", 20)
